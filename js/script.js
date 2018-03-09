7850d896ef3fe9baf24ca87305a334d45c6c41d9
Nathalia Buitrago J <nathalia.buitragoj@gmail.com>
2018-03-09 17:58:18 +0000

    Techdegree project 4

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    // Variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('gallery');
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1 || $(a).data("title") === filter.toLocaleLowerCase()) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
